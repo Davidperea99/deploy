cf41f355e35876a1e801f8d76f6dc41129d401d3
Unicomfacauca <unicomfacauca@PPN-MAC-PC20.local>
2024-09-28 16:17:55 -0500

    first commit

--- routers/routerCultivos.py
+++ routers/routerCultivos.py
@@ -35,7 +35,7 @@ def predict_cultivos(data:schemas.Cultivosdata):
 
 
 
-    xin = np.array([N,P,K,Temperature,Humidity,ph,rainfall]).reshape(1,8)
+    xin = np.array([N,P,K,Temperature,Humidity,ph,rainfall]).reshape(1,7)
 
     prediction = model.predict(xin)
     yout = labels[prediction[0]]
